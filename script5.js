24d523710272973cd6c13f678647bd34d948de6d
HanumGoo <sheehanandya001@gmail.com>
2026-07-24 13:39:52 +0700

    update another pls

--- script5.js
+++ script5.js
@@ -6,6 +6,30 @@ const resulter = document.getElementById("content-result");
 const allOutcity = document.querySelectorAll(".optional-outcity");
 const weightChange = document.getElementById("content-weightChange");
 
+const cityPrice = [
+  {
+    cityName: "bogor",
+    priceKG: 2000,
+  },
+  {
+    cityName: "depok",
+    priceKG: 4000,
+  },
+  {
+    cityName: "jakarta",
+    priceKG: 6000,
+  },
+  {
+    cityName: "bandung",
+    priceKG: 6000,
+  },
+  {
+    cityName: "bekasi",
+    priceKG: 6000,
+  },
+];
+console.log(cityPrice[0]);
+
 button.addEventListener("click", () => {
   if (city.value == "none") {
     alert("pilih kota dulu");
@@ -33,16 +57,22 @@ button.addEventListener("click", () => {
     if (outcity.value == "") {
       alert("minimal masukin kota dulu");
       return;
+    } else {
+      if (cityPrice.some((x) => x.cityName == outcity.value.trim().toLowerCase())) {
+        let weightContent;
+        if (weightChange.value == "gr") {
+          weightContent = weight.value / 1000;
+        } else if (weightChange.value == "kg") {
+          weightContent = weight.value;
+        }
+
+        const pricePerKG = cityPrice.find((x) => x.cityName == outcity.value.trim().toLowerCase()).priceKG ?? 0;
+        const total = weightContent * pricePerKG;
+        resulter.innerHTML = "Total Biaya : IDR " + total.toLocaleString("en-ID");
+      } else {
+        resulter.innerHTML = "Total Biaya : (Kota yang kamu masukkan tidak support pelayanan)";
+      }
     }
-    let weightContent;
-    if (weightChange.value == "gr") {
-      weightContent = weight.value / 1000;
-    } else if (weightChange.value == "kg") {
-      weightContent = weight.value;
-    }
-    const pricePerKG = 6000;
-    const total = weightContent * pricePerKG;
-    resulter.innerHTML = "Total Biaya : IDR " + total.toLocaleString("en-ID");
   }
 });
 
